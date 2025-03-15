dba5e6eb5809811e20c519c73edaddbf2aaa2b61
Nolucci <112586749+Nolucci@users.noreply.github.com>
2025-03-15 01:45:14 +0100

    feat: improve NDI.js

--- src/components/Projects/Pages/NDI.js
+++ src/components/Projects/Pages/NDI.js
@@ -46,11 +46,12 @@ function NDI() {
                         <p style={{ color: "white", textAlign: "justify" }}>
                             La Nuit de l'Info est un événement national où des équipes d'étudiants disposent de 16 heures pour concevoir un site web répondant à un défi commun et à des défis annexes proposés par des entreprises. En 2023, le thème principal était le <em>Réchauffement climatique</em>. Notre équipe initialement constituée de 10 personnes a relevé divers défis techniques et organisationnels, travaillant intensément pour livrer un produit fonctionnel et innovant dans un délai très court.
                         </p>
-                        <h4 className="text-white">Points clés du projet :</h4>
+                        <br/>
+                        <h4 className="text-white">Points clés réalisés :</h4>
                         <ListGroup variant="flush" className="text-white text-start">
                             <ListGroup.Item className="bg-dark text-white border-0">Développement rapide et efficace sous forte contrainte temporelle</ListGroup.Item>
-                            <ListGroup.Item className="bg-dark text-white">Coordination d'équipe (organisation en sous-groupes, distribution des tâches)</ListGroup.Item>
-                            <ListGroup.Item className="bg-dark text-white">Support technique aux membres de l'équipe (résolution de problèmes techniques, formation des nouveaux)</ListGroup.Item>
+                            <ListGroup.Item className="bg-dark text-white">Coordination d'équipe et répartition des tâches</ListGroup.Item>
+                            <ListGroup.Item className="bg-dark text-white">Support technique aux membres de l'équipe et optimisation du site</ListGroup.Item>
                         </ListGroup>
                     </Col>
                     <Col md={5}>
@@ -62,7 +63,7 @@ function NDI() {
                     <Col md={7}>
                         <h2 className="blue mb-3">Ma contribution personnelle</h2>
                         <p style={{ color: "white", textAlign: "justify" }}>
-                            Durant la Nuit de l'Info 2023, j'ai eu un rôle clé en organisant et en répartissant le travail entre les 8 membres actifs de l'équipe, formant notamment des binômes efficaces. J'ai pris part activement au développement Frontend en HTML, CSS et JavaScript, et j'ai géré la mise en place de plusieurs fonctionnalités interactives du site. J'ai également assuré un support technique aux membres de l'équipe moins expérimentés.
+                            J'ai organisé et coordonné les efforts de l'équipe en distribuant les tâches et en formant des binômes efficaces. En parallèle, j'ai travaillé sur le développement Frontend en HTML, CSS et JavaScript, et j'ai apporté un support technique aux membres de l'équipe moins expérimentés.
                         </p>
                         <div className="mt-4">
                             <Button variant="outline-light" href="https://gitlabinfo.iutmontp.univ-montp2.fr/franceskinn/ndi-2023" target="_blank">
