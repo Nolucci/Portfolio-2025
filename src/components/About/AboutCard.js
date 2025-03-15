29bc07bcf4e1e049c249d3227bca1c13ea06e65a
Nolucci <112586749+Nolucci@users.noreply.github.com>
2025-03-15 13:38:04 +0100

    improve about + images

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -4,34 +4,44 @@ import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
   return (
-    <Card className="quote-card-view">
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Salut tout le monde ! Je viens du sud de la France : <span className="blue">Montpellier.</span>
+      <Card className="quote-card-view">
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
+            <p style={{ textAlign: "justify" }}>
+              Salut tout le monde ! Je viens du sud de la France : <span className="blue">Montpellier.</span>
+              <br />
+              <br />
+              Passionné par le développement web et les nouvelles technologies, j'adore explorer
+              les dernières tendances en programmation et partager mes découvertes avec la communauté.
+              <br />
+              <br/>
+              Dans mon temps libre, j'aime faire de la musique et de la robotique et me plonger dans
+              de nouveaux projets intéressants. La psychologie et la sociologie sont deux autres de mes passions !
+              <br />
+              <br />
+              Voici quelques-unes de mes activités favorites :
+            </p>
+            <ul>
+              <li className="about-activity">
+                <ImPointRight /> Développement de mes projets personnels
+              </li>
+              <br />
+              <li className="about-activity">
+                <ImPointRight /> En apprendre plus sur les nouvelles technologies
+              </li>
+              <br />
+              <li className="about-activity">
+                <ImPointRight /> En apprendre plus sur l'Humain
+              </li>
+            </ul>
             <br />
-            <br />
-            <br />
-            <br />
-          </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight />
-            </li>
-            <li className="about-activity">
-              <ImPointRight />
-            </li>
-            <li className="about-activity">
-              <ImPointRight />
-            </li>
-          </ul>
-
-          <p style={{ color: "rgb(155 126 172)" }}>{" "}
-          </p>
-          <footer className="blockquote-footer"></footer>
-        </blockquote>
-      </Card.Body>
-    </Card>
+            <p className="blockquote-footer" >
+              "La technologie seule ne suffit pas."
+            </p>
+            <footer style={{ color: "rgb(155 126 172)" }} className="fst-italic">Steve JOBS</footer>
+          </blockquote>
+        </Card.Body>
+      </Card>
   );
 }
 
