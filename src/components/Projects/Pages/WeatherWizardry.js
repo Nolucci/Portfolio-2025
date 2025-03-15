17a63b406091557c256a3de5242bf6fc91653122
Nolucci <112586749+Nolucci@users.noreply.github.com>
2025-03-15 01:46:11 +0100

    feat: improve WW.js

--- src/components/Projects/Pages/WeatherWizardry.js
+++ src/components/Projects/Pages/WeatherWizardry.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col, Card, Image, Button } from "react-bootstrap";
 import Particle from "../../Particle";
-import ww from "../../../Assets/Projects/WeatherWizardry.png"; // Vérifie ton chemin
+import ww from "../../../Assets/Projects/WeatherWizardry.png";
 import { FaExternalLinkAlt } from "react-icons/fa";
 
 function WeatherWizardry() {
@@ -12,7 +12,7 @@ function WeatherWizardry() {
                 <Row className="align-items-center mb-5">
                     <Col md={7}>
                         <h1 className="project-heading mb-4">Weather Wizardry : Maître du temps</h1>
-                        <Card className="bg-dark text-white shadow">
+                        <Card className="bg-dark text-white shadow rounded-3">
                             <Card.Body>
                                 <Row>
                                     <Col md={6} className="mb-3">
@@ -44,7 +44,7 @@ function WeatherWizardry() {
                     <Col md={7}>
                         <h2 className="blue mb-3">Description du projet</h2>
                         <p style={{ color: "white", textAlign: "justify" }}>
-                            Première participation à la Code Game Jam, une compétition de développement de jeux vidéo en 2 jours. Malgré une équipe réduite de moitié à cause d'absences et des difficultés techniques majeures, nous avons réussi à créer un jeu de type <strong>platformer</strong> basé sur le thème « Maître du temps ». Grâce à une organisation rigoureuse et une détermination forte, notre équipe a décroché le <strong>troisième prix 🥉</strong>. Ce projet m'a permis de renforcer mes compétences techniques en C# et Unity, ainsi que mes capacités à gérer efficacement une crise et un projet sous contrainte (AC 6 et AC 10).
+                            Première participation à la Code Game Jam, une compétition de développement de jeux vidéo en 2 jours. Malgré une équipe réduite de moitié à cause d'absences et des difficultés techniques majeures, nous avons réussi à créer un jeu de type <strong>platformer</strong> basé sur le thème « Maître du temps ». Grâce à une organisation rigoureuse et une détermination forte, notre équipe a décroché le <strong>troisième prix 🥉</strong>. Ce projet m'a permis de renforcer mes compétences techniques en C# et Unity, ainsi que mes capacités à gérer efficacement une crise et un projet sous contrainte.
                         </p>
                         <ul className="text-white text-start">
                             <li>Développement intensif des mécaniques de gameplay (collisions, interactions)</li>
